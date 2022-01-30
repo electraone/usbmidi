db76dd524b077d0e88c9d6fea8d301d53ced3236
Martin Pavlas <martin@think-forth.com>
2022-01-30 23:30:01 +0100

    Blink leds in the button_read thread

--- src/read_buttons_thread_entry.c
+++ src/read_buttons_thread_entry.c
@@ -6,6 +6,46 @@ void read_buttons_thread_entry(void)
     /* TODO: add your own code here */
     while (1)
     {
-        tx_thread_sleep (1);
+        /* Define the units to be used with the threadx sleep function */
+        const uint32_t threadx_tick_rate_Hz = 100;
+        /* Set the blink frequency (must be <= threadx_tick_rate_Hz */
+        const uint32_t freq_in_hz = 2;
+        /* Calculate the delay in terms of the threadx tick rate */
+        const uint32_t delay = threadx_tick_rate_Hz/freq_in_hz;
+        /* LED type structure */
+        bsp_leds_t leds;
+        /* LED state variable */
+        ioport_level_t level = IOPORT_LEVEL_HIGH;
+
+        /* Get LED information for this board */
+        R_BSP_LedsGet(&leds);
+
+        /* If this board has no leds then trap here */
+        if (0 == leds.led_count)
+        {
+            while(1);   // There are no leds on this board
+        }
+
+        while (1)
+        {
+            /* Determine the next state of the LEDs */
+            if(IOPORT_LEVEL_LOW == level)
+            {
+                level = IOPORT_LEVEL_HIGH;
+            }
+            else
+            {
+                level = IOPORT_LEVEL_LOW;
+            }
+
+            /* Update all board LEDs */
+            for(uint32_t i = 0; i < leds.led_count; i++)
+            {
+                g_ioport.p_api->pinWrite(leds.p_leds[i], level);
+            }
+
+            /* Delay */
+            tx_thread_sleep (delay);
+        }
     }
 }
